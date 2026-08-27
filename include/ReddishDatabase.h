2245760dee66f39474a161c3da8b369e98bd09bc
Kalp Shah <kalps5282@gmail.com>
2026-08-27 19:31:07 +0000

    added basic map based data structures

--- include/ReddishDatabase.h
+++ include/ReddishDatabase.h
@@ -1,18 +1,29 @@
 #ifndef REDDISH_DATABASE_H
 #define REDDISH_DATABASE_H
 #include <string>
+#include <mutex>
+#include <unordered_map>
+#include <vector>
+
 class ReddishDatabase {
     private:
         ReddishDatabase()=default;
         ~ReddishDatabase()=default;
         ReddishDatabase(const ReddishDatabase&) = delete;
         ReddishDatabase& operator=(const ReddishDatabase&) = delete;
+        std::mutex db_mutex;
+        std::unordered_map<std::string,std::string> key_value_store;
+        std::unordered_map<std::string,std::unordered_map<std::string,std::string>> hash_store;
+        std::unordered_map<std::string,std::vector<std::string>> list_store;
+
     public:
         //this is singleton instances so no other copy or assignment is allowed
         static ReddishDatabase& getInstance();
         //persistence
         bool dump(const std::string& filename);
         bool load(const std::string& filename);
+
+
 };
 
 
--- src/ReddishDatabase.cpp
+++ src/ReddishDatabase.cpp
@@ -1,5 +1,8 @@
 #include "ReddishDatabase.h"
 #include <iostream>
+#include <mutex>
+#include <fstream>
+#include <sstream>
 
 ReddishDatabase& ReddishDatabase::getInstance(){
     static ReddishDatabase instance;
@@ -7,9 +10,87 @@ ReddishDatabase& ReddishDatabase::getInstance(){
 }
 
 bool ReddishDatabase::dump(const std::string& filename){
+    std::lock_guard<std::mutex> lock(db_mutex);
+    std::ofstream ofs(filename,std::ios::binary);
+    if(!ofs){
+        std::cerr<<"Failed to open file "<<filename<<" for writing"<<std::endl;
+        return false;
+    }
+    for(const auto& pair:key_value_store){
+        ofs<<"K"<<" "<<pair.first.size()<<" "<<pair.first<<" "<<pair.second.size()<<" "<<pair.second<<"\n";
+    }
+    for(const auto& pair:hash_store){
+        ofs<<"H"<<" "<<pair.first.size()<<" "<<pair.first<<" "<<pair.second.size()<<"\n";
+        for(const auto& inner_pair:pair.second){
+            ofs<<inner_pair.first.size()<<" "<<inner_pair.first<<" "<<inner_pair.second.size()<<" "<<inner_pair.second<<"\n";
+        }
+    }
+    for(const auto& pair:list_store){
+        ofs<<"L"<<" "<<pair.first.size()<<" "<<pair.first<<" "<<pair.second.size()<<"\n";
+        for(const auto& value:pair.second){
+            ofs<<value.size()<<" "<<value<<"\n";
+        }
+    }
     return true;
 }
 
 bool ReddishDatabase::load(const std::string& filename){
+    std::lock_guard<std::mutex> lock(db_mutex);
+    std::ifstream ifs(filename,std::ios::binary);
+    if(!ifs){
+        std::cerr<<"Failed to open file "<<filename<<" for reading"<<std::endl;
+        return false;
+    }
+    key_value_store.clear();
+    hash_store.clear();
+    list_store.clear();
+    std::string line;
+    while(std::getline(ifs,line)){
+        std::istringstream iss(line,std::ios::binary);
+        char type;
+        iss>>type;
+        if(type=='K'){
+            size_t key_size,value_size;
+            std::string key,value;
+            iss>>key_size>>key>>value_size>>value;
+            key_value_store[key]=value;
+        }
+        else if(type=='H'){
+            size_t key_size,hash_size;
+            std::string key;
+            iss>>key_size>>key>>hash_size;
+            std::unordered_map<std::string,std::string> hash_map;
+            for(size_t i=0;i<hash_size;i++){
+                size_t field_size,value_size;
+                std::string field,value;
+                if(!std::getline(ifs,line)){
+                    std::cerr<<"Unexpected end of file while reading hash"<<std::endl;
+                    return false;
+                }
+                std::istringstream field_iss(line);
+                field_iss>>field_size>>field>>value_size>>value;
+                hash_map[field]=value;
+            }
+            hash_store[key]=hash_map;
+        }
+        else if(type=='L'){
+            size_t key_size,list_size;
+            std::string key;
+            iss>>key_size>>key>>list_size;
+            std::vector<std::string> list_values;
+            for(size_t i=0;i<list_size;i++){
+                size_t value_size;
+                std::string value;
+                if(!std::getline(ifs,line)){
+                    std::cerr<<"Unexpected end of file while reading list"<<std::endl;
+                    return false;
+                }
+                std::istringstream value_iss(line);
+                value_iss>>value_size>>value;
+                list_values.push_back(value);
+            }
+            list_store[key]=list_values;
+        }
+    }
     return true;
 }
